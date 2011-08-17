41b1556636aa4f1a2661c84a090f4c1905cfada8
Fabien Franzen <info@atelierfabien.be>
2011-08-17 19:02:18 +0200

    Refactored Product stock messages/status

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -6,10 +6,31 @@ Product.class_eval do
   
   def stock_status_message
     if has_stock?
-      I18n.t('simple_availability.in_stock')[in_stock_status]
+      in_stock_status_message
     else
-      I18n.t('simple_availability.out_of_stock')[out_of_stock_status]
+      out_of_stock_status_message
     end    
+  end
+  
+  def in_stock_status_message
+    I18n.t('simple_availability.in_stock')[in_stock_status]
+  end
+  
+  def out_of_stock_status_message
+    I18n.t('simple_availability.out_of_stock')[out_of_stock_status]
+  end
+  
+  def stock_status_options
+    { 
+      :in_stock => { 
+        :msg => in_stock_status_message, 
+        :status => in_stock_status
+      },
+      :out_of_stock => { 
+        :msg => out_of_stock_status_message,
+        :status => out_of_stock_status
+      }
+    }
   end
   
   def self.in_stock_options
